12e2641206eebf046bdcae4ebfa674b8c4a27e40
nickyuan <nickyuan7@gmail.com>
2024-09-10 12:30:09 +1000

    Initial Commit

---
+++ pushNotificationTest/pushNotificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  pushNotificationTestApp.swift
+//  pushNotificationTest
+//
+//  Created by Zi Fan on 10/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct pushNotificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
